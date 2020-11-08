fd0b8f6b068091698ecffce2cd9a3f6ca257a6b9
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-11-09 00:09:26 +0900

    サムネイル表示する関数とサイズ整える関数を追加

--- wp-content/themes/easiest-wp/functions.php
+++ wp-content/themes/easiest-wp/functions.php
@@ -10,6 +10,14 @@ add_action('wp_enqueue_scripts', 'easiestwp_scripts');
 // タイトルを読み込むやつ
 function easiestwp_setup(){
   add_theme_support('title-tag');
+
+  // サムネイルを表示するやつ
+  add_theme_support('post-thumbnails');
+
+// 画像のサイズを調整するやつ
+  add_image_size('easiestwp-thumbnails', 190, 130);
+  add_image_size('easiestwp-hero', 1200, 630);
+  
 }
 
 add_action('after_setup_theme', 'easiestwp_setup');
