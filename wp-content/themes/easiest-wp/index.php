544b2ef253b0ba9f273b09cb9354578a37b49092
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-11-08 21:04:22 +0900

    ページネーションを追加
    URLとかが変わってしまうため、
    get_theme_file_uri() でURLを取得

--- wp-content/themes/easiest-wp/index.php
+++ wp-content/themes/easiest-wp/index.php
@@ -62,7 +62,10 @@
 					<a href="index.html"><img class="arrow" src="images/arrow-right.png" srcset="images/arrow-right@2x.png 2x" alt="次へ"></a>
 				</div>
 			</nav> -->
-			<?php the_posts_pagination(); ?>
+			<?php the_posts_pagination(array(
+				'prev_text' => '<img class="arrow" src="'.get_theme_file_uri().'/images/arrow-left.png" srcset="'.get_theme_file_uri().'/images/arrow-left@2x.png 2x" alt="前へ">',
+				'next_text' => '<img class="arrow" src="'.get_theme_file_uri().'/images/arrow-right.png" srcset="'.get_theme_file_uri().'/images/arrow-right@2x.png 2x" alt="次へ">'
+			)); ?>
 
 			
 
