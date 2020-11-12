ab93e9d3ad604e094b4fb9ac107b3e7516e1ef43
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-11-13 00:52:26 +0900

    header.phpを作成

--- wp-content/themes/easiest-wp/index.php
+++ wp-content/themes/easiest-wp/index.php
@@ -10,34 +10,8 @@
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-	<header class="page-header">
-		<div class="header-area">
-			<div class="panel-site-title">
-				<p class="site-title"><a href=<?php echo esc_url(home_url()); ?>><?php bloginfo('name'); ?></a></p>
-				<p class="site-subtitle"><?php bloginfo('description'); ?></p>
-			</div>
-
-			<!-- <nav class="global-nav">
-				<ul id="global-menu" class="menu">
-					<li class="current-menu-item"><a href="index.html">ホーム</a></li>
-					<li><a href="portfolio.html">ポートフォリオ</a></li>
-					<li><a href="profile.html">プロフィール</a></li>
-					<li><a href="contact.html">お問い合わせ</a></li>
-				</ul>
-			</nav> -->
-
-			<!-- グローバルメニューを表示 -->
-			<?php if (has_nav_menu('global')):?>
-				<?php wp_nav_menu(array(
-					'theme_location' => 'global',
-					'menu_id' =>'global-menu',
-					'container' => 'nav',
-					'container_class' => 'global-nav'
-				)); ?>
-			<?php endif; ?>
-
-		</div>
-	</header>
+	
+	<?php get_header(); ?>
 
 	<div class="hero"></div>
 	<div class="content-area has-side-col">
