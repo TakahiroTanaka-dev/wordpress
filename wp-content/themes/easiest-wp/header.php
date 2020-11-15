14bc7eaa14635a7895c09c0682307b17d3715309
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-11-15 20:29:01 +0900

    ヘッダー追加

--- wp-content/themes/easiest-wp/header.php
+++ wp-content/themes/easiest-wp/header.php
@@ -1,3 +1,15 @@
+<!DOCTYPE HTML>
+<!-- wordpressにひっす -->
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset=<?php bloginfo('charset'); ?>>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- <link rel="stylesheet" href="style.css" /> -->
+	<!-- ヘッダーのフック -->
+	<?php	wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
 <header class="page-header">
 		<div class="header-area">
 			<div class="panel-site-title">
