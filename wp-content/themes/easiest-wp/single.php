85ede6c715a00f973c32d46b35e1abc3b576bfb0
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-11-15 20:34:51 +0900

    single.phpを追加

---
+++ wp-content/themes/easiest-wp/single.php
@@ -0,0 +1,185 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Easiest WP</title>
+	<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<header class="page-header">
+		<div class="header-area">
+			<div class="panel-site-title">
+				<p class="site-title"><a href="index.html">Easiest WP</a></p>
+				<p class="site-subtitle">Just another WordPress site</p>
+			</div>
+
+			<nav class="global-nav">
+				<ul id="global-menu" class="menu">
+					<li class="current-menu-item"><a href="index.html">ホーム</a></li>
+					<li><a href="portfolio.html">ポートフォリオ</a></li>
+					<li><a href="profile.html">プロフィール</a></li>
+					<li><a href="contact.html">お問い合わせ</a></li>
+				</ul>
+			</nav>
+
+		</div>
+	</header>
+
+	<div class="hero eyecatch">
+		<img src="http://placehold.it/1200x630">
+	</div>
+
+	<div class="content-area has-side-col">
+		<div class="main-column">
+			<div class="box-generic">
+				<h1 class="box-heading box-heading-article">吾輩は猫である。名前はまだ無い。</h1>
+				<div class="box-content">
+
+					<article class="entry">
+						<h2>見出しその2</h2>
+						<h3>見出しその3</h3>
+						<h4>見出しその4</h4>
+						<h5>見出しその5</h5>
+						<h6>見出しその6</h6>
+						<p><strong>どこで生れたかとんと見当がつかぬ。</strong>何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。</p>
+						<p><a href="#">しかしその当時は何という考もなかったから別段恐しいとも思わなかった。</a>ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少し落ちついて書生の顔を見たのがいわゆる人間というものの見始であろう。この時妙なものだと思った感じが今でも残っている。第一毛をもって装飾されべきはずの顔がつるつるしてまるで薬缶だ。その後猫にもだいぶ逢ったがこんな片輪には一度も出会わした事がない。のみならず顔の真中があまりに突起している。そうしてその穴の中から時々ぷうぷうと煙を吹く。どうも咽せぽくて実に弱った。これが人間の飲む煙草というものである事はようやくこの頃知った。</p>
+						<p>この書生の掌の裏でしばらくはよい心持に坐っておったが、しばらくすると非常な速力で運転し始めた。書生が動くのか自分だけが動くのか分らないが無暗に眼が廻る。胸が悪くなる。到底助からないと思っていると、どさりと音がして眼から火が出た。それまでは記憶しているがあとは何の事やらいくら考え出そうとしても分らない。</p>
+						<ul>
+							<li>リスト1</li>
+							<li>リスト2</li>
+							<li>リスト3</li>
+							<li>リスト4</li>
+						</ul>
+						<p>ふと気が付いて見ると書生はいない。たくさんおった兄弟が一疋も見えぬ。肝心の母親さえ姿を隠してしまった。その上今までの所とは違って無暗に明るい。眼を明いていられぬくらいだ。はてな何でも容子がおかしいと、のそのそ這い出して見ると非常に痛い。吾輩は藁の上から急に笹原の中へ棄てられたのである。</p>
+					</article>
+
+					<div class="meta-data">
+						<time class="meta meta-entry-date" datetime="2017-04-01T23:59:99+09:00">2017年4月1日</time>
+						<p class="meta meta-author"><a href="archive.html">著者名</a></p>
+						<p class="meta meta-cat"><a href="archive.html">カテゴリ名</a></p>
+						<p class="meta meta-tag">タグ: <a href="archive.html">タグ名</a>, <a href="archive.html">タグ名</a>, <a href="archive.html">タグ名</a>, <a href="archive.html">タグ名</a></p>
+					</div>
+
+					<nav class="navigation post-navigation" role="navigation">
+						<div class="nav-links">
+							<div class="nav-previous"><a href="single.html">前の記事：記事タイトル記事タイトル記事タイトル記事タイトル</a></div>
+							<div class="nav-next"><a href="single.html">次の記事：記事タイトル記事タイトル記事タイトル記事タイトル</a></div>
+						</div>
+					</nav>
+				</div>
+			</div>
+
+			<div class="box-generic">
+				<div class="box-content box-comment-display">
+					<h2>コメントとトラックバック</h2>
+					<ul class="comment-list">
+						<li id="comment-1" class="comment even thread-even depth-1 parent">
+							<article id="div-comment-1" class="comment-body">
+								<footer class="comment-meta">
+									<div class="comment-author vcard">
+										<img class="avatar" src="http://placehold.it/130x130">
+										<b class="fn"><a href="#" rel="external nofollow" class="url">WordPress コメントの投稿者</a></b> <span class="says">より:</span>
+									</div><!-- .comment-author -->
+									<div class="comment-metadata">
+										<a href="#">
+											<time datetime="2017-04-16T23:40:30+00:00">2017年4月16日 11:40 PM	</time>
+										</a>
+										<span class="edit-link"><a class="comment-edit-link" href="#">編集</a></span>
+									</div><!-- .comment-metadata -->
+								</footer><!-- .comment-meta -->
+								<div class="comment-content">
+									<p>こんにちは、これはコメントです。<br>
+									コメントの承認、編集、削除を始めるにはダッシュボードの「コメント画面」にアクセスしてください。<br>
+									コメントのアバターは「<a href="https://gravatar.com">Gravatar</a>」から取得されます。</p>
+								</div><!-- .comment-content -->
+								<div class="reply"><a rel="nofollow" class="comment-reply-link" href="#" aria-label="WordPress コメントの投稿者 に返信">返信</a></div>
+							</article><!-- .comment-body -->
+							<ul class="children">
+								<li id="comment-3" class="comment byuser comment-author-admin bypostauthor odd alt depth-2">
+									<article id="div-comment-3" class="comment-body">
+										<footer class="comment-meta">
+											<div class="comment-author vcard">
+												<img class="avatar" src="http://placehold.it/130x130">
+												<b class="fn">admin</b> <span class="says">より:</span>
+											</div><!-- .comment-author -->
+											<div class="comment-metadata">
+												<a href="#">
+													<time datetime="2017-04-18T17:45:33+00:00">2017年4月18日 5:45 PM</time>
+												</a>
+												<span class="edit-link"><a class="comment-edit-link" href="#">編集</a></span>
+											</div><!-- .comment-metadata -->
+										</footer><!-- .comment-meta -->
+										<div class="comment-content">
+											<p>こんにちは、これはコメントです。<br />
+											コメントの承認、編集、削除を始めるにはダッシュボードの「コメント画面」にアクセスしてください。<br />
+											コメントのアバターは「Gravatar」から取得されます。</p>
+										</div><!-- .comment-content -->
+										<div class="reply"><a rel="nofollow" class="comment-reply-link" href="#" aria-label="admin に返信">返信</a></div>
+									</article><!-- .comment-body -->
+								</li><!-- #comment-## -->
+							</ul><!-- .children -->
+						</li><!-- #comment-## -->
+					</ul>
+				</div>
+			</div>
+			<div class="box-generic">
+				<div class="box-content box-comment-input">
+					<h3 id="reply-title" class="comment-reply-title">コメントを残す</h3>
+					<p class="comment-notes">メールアドレスが公開されることはありません。 * が付いている欄は必須項目です</p>
+					<form action="#" class="comment-form">
+						<p class="comment-notes"></p>
+					</form>
+				</div>
+			</div>
+
+		</div>
+
+		<ul class="side-column">
+			<li class="widget">
+				<form class="searchform">
+					<div>
+						<input type="text">
+						<input value="検索" type="submit">
+					</div>
+				</form>
+			</li>
+			<li class="widget">
+				<h2 class="widgettitle">最近の投稿</h2>
+				<ul>
+					<li><a href="single.html">記事タイトル記事タイトル記事タイトル記事タイトル</a></li>
+					<li><a href="single.html">記事タイトル記事タイトル記事タイトル記事タイトル</a></li>
+					<li><a href="single.html">記事タイトル記事タイトル記事タイトル記事タイトル</a></li>
+					<li><a href="single.html">記事タイトル記事タイトル記事タイトル記事タイトル</a></li>
+					<li><a href="single.html">記事タイトル記事タイトル記事タイトル記事タイトル</a></li>
+				</ul>
+			</li>
+			<li class="widget">
+				<h2 class="widgettitle">カテゴリー</h2>
+				<ul>
+					<li><a href="archive.html">カテゴリ名</a></li>
+					<li><a href="archive.html">カテゴリ名</a></li>
+					<li><a href="archive.html">カテゴリ名</a></li>
+				</ul>
+			</li>
+		</ul>
+
+	</div>
+
+	<footer class="page-footer">
+		<div class="footer-widget-area">
+			<ul class="footer-widgets">
+				<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
+				<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
+				<li><a href="#"><img src="http://placehold.it/320x80"></a></li>
+			</ul>
+			<div class="back-to-top">
+				<a href="#"><img src="images/arrow-up.png" srcset="images/arrow-up@2x.png 2x" alt="">TOP</a>
+			</div>
+		</div>
+		<div class="copyright">
+			<p>Copyright ©  Gijutsu-Hyohron Co., Ltd.</p>
+		</div>
+	</footer>
+</body>
+</html>
