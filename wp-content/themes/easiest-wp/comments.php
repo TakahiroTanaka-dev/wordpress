098d5ff4c2b63a7046c4947e8ee41edfe4628ec0
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-11-15 23:32:18 +0900

    元々コメントがあったsingle.phpのhtml部分をphpに変換し、comments.phpの多くをphpに変更

---
+++ wp-content/themes/easiest-wp/comments.php
@@ -0,0 +1,21 @@
+<?php if (have_comments()) : ?>
+  <div class="box-generic">
+    <div class="box-content box-comment-display">
+      <h2>コメントとトラックバック</h2>
+      <ul class="comment-list">
+        <?php wp_list_comments(array(
+          'avatar_size' => 130,
+        )) ?>
+      </ul>
+    </div>
+  </div>
+<?php endif; ?>
+<div class="box-generic">
+  <div class="box-content box-comment-input">
+    <h3 id="reply-title" class="comment-reply-title">コメントを残す</h3>
+    <p class="comment-notes">メールアドレスが公開されることはありません。 * が付いている欄は必須項目です</p>
+    <form action="#" class="comment-form">
+      <p class="comment-notes"></p>
+    </form>
+  </div>
+</div>
