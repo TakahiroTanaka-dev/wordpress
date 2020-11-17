8fda4b265722577c8d93b130d8fa2cccc3fe5542
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-11-17 23:53:07 +0900

    get_theme_modを使用して、front_page_contentが1〜5まであるかを判定

--- wp-content/themes/easiest-wp/front-page.php
+++ wp-content/themes/easiest-wp/front-page.php
@@ -18,38 +18,42 @@
 	<div class="hero"></div>
 	<div class="content-area has-side-col">
 		<div class="main-column">
+			<!-- for分でメインコンテンツを5回分繰り返す -->
 			<?php for ($i = 1; $i <= 5; $i++) : ?>
-				<h1 class="box-heading box-heading-main-col">Blog</h1>
-				<div class="box-content">
+				<!-- get_theme_modでfront_page_content_1~5に登録されているかどうかを判定する -->
+				<?php if (get_theme_mod('front_page_content' . $i)) : ?>
+					<h1 class="box-heading box-heading-main-col">Blog</h1>
+					<div class="box-content">
 
-					<!-- こっから記事があれば表示される -->
-					<?php if (have_posts()) : ?>
-						<ul class="archive">
+						<!-- こっから記事があれば表示される -->
+						<?php if (have_posts()) : ?>
+							<ul class="archive">
 
-							<!-- こっからループ処理 -->
-							<?php while (have_posts()) : ?>
-								<?php the_post(); ?>
-								<li class="item-archive">
-									<div class="time-and-thumb-archive">
-										<time class="pub-date" datetime=<?php echo get_the_date('DATE_W3C') ?>><?php echo get_the_date() ?></time>
+								<!-- こっからループ処理 -->
+								<?php while (have_posts()) : ?>
+									<?php the_post(); ?>
+									<li class="item-archive">
+										<div class="time-and-thumb-archive">
+											<time class="pub-date" datetime=<?php echo get_the_date('DATE_W3C') ?>><?php echo get_the_date() ?></time>
 
-										<!-- PHP使ってループ処理の中でサムネイルを適用した -->
-										<?php if (has_post_thumbnail()) : ?>
-											<p class="thumb thumb-archive"><a href=<?php the_permalink(); ?>><?php the_post_thumbnail('easiestwp-thumbnail') ?></a></p>
-										<?php endif; ?>
+											<!-- PHP使ってループ処理の中でサムネイルを適用した -->
+											<?php if (has_post_thumbnail()) : ?>
+												<p class="thumb thumb-archive"><a href=<?php the_permalink(); ?>><?php the_post_thumbnail('easiestwp-thumbnail') ?></a></p>
+											<?php endif; ?>
 
-									</div>
-									<div class="data-archive">
-										<p class="list-categories-archive"><?php the_category(','); ?></p>
-										<h2 class="title-archive"><a href=<?php the_permalink(); ?>><?php the_title(); ?></a></h2>
-										<p class="list-tags-archive"><?php the_tags(); ?></p>
-									</div>
-								<?php endwhile; ?>
-						</ul>
-					<?php else : ?>
-						<p>投稿はありません</p>
-					<?php endif; ?>
-				</div>
+										</div>
+										<div class="data-archive">
+											<p class="list-categories-archive"><?php the_category(','); ?></p>
+											<h2 class="title-archive"><a href=<?php the_permalink(); ?>><?php the_title(); ?></a></h2>
+											<p class="list-tags-archive"><?php the_tags(); ?></p>
+										</div>
+									<?php endwhile; ?>
+							</ul>
+						<?php else : ?>
+							<p>投稿はありません</p>
+						<?php endif; ?>
+					</div>
+						<?php endif; ?>
 			<?php endfor; ?>
 
 			<!-- 最下部のページネーションを表示 -->
