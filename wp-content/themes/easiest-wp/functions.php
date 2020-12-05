231bb7664a15d499c47c69679c9c065f66f341a6
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-12-05 15:11:55 +0900

    ブロックパターンを作成
    3カラムを持ったブロックパターンを作成

--- wp-content/themes/easiest-wp/functions.php
+++ wp-content/themes/easiest-wp/functions.php
@@ -85,4 +85,53 @@ function easiestwp_front_page_template($template){
   return is_home() ? '' : $template;
 }
 
+if(function_exists('register_block_pattern')){
+  function my_block_pattern(){
+
+    $block_pattern_html = '<!-- wp:columns {"align":"wide"} -->
+    <div class="wp-block-columns alignwide">
+      <!-- wp:column -->
+        <div class="wp-block-column">
+   
+          <!-- wp:paragraph {"textColor":"black","fontSize":"medium"} -->
+            <p class="has-black-color has-text-color has-medium-font-size"><strong>吾輩は猫である</strong></p>
+          <!-- /wp:paragraph -->
+          <!-- wp:paragraph {"textColor":"black","fontSize":"small"} -->
+            <p class="has-black-color has-text-color has-small-font-size">吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。</p>
+          <!-- /wp:paragraph -->
+   
+        </div>
+      <!-- /wp:column -->
+   
+      <!-- wp:column -->
+        <div class="wp-block-column">
+          <!-- wp:paragraph {"textColor":"black","fontSize":"small"} -->
+            <p class="has-black-color has-text-color has-small-font-size">この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少し落ちついて書生の顔を見たのがいわゆる人間というものの見始であろう。</p>
+          <!-- /wp:paragraph -->
+        </div>
+      <!-- /wp:column -->
+   
+      <!-- wp:column -->
+        <div class="wp-block-column">
+          <!-- wp:paragraph {"textColor":"black","fontSize":"small"} -->
+            <p class="has-black-color has-text-color has-small-font-size">この時妙なものだと思った感じが今でも残っている。第一毛をもって装飾されべきはずの顔がつるつるしてまるで薬缶だ。その後猫にもだいぶ逢ったがこんな片輪には一度も出会わした事がない。のみならず顔の真中があまりに突起している。そうしてその穴の中から時々ぷうぷうと煙を吹く。どうも咽せぽくて実に弱った。</p>
+          <!-- /wp:paragraph -->
+        </div>
+      <!-- /wp:column -->
+    </div>
+  <!-- /wp:columns -->
+  ';
+    register_block_pattern(
+      'three_columns_text',
+      array(
+        'title' => '3カラムテキスト',
+        'category' => array('columns'),
+        'content' => $block_pattern_html,
+      )
+      );
+  }
+
+  add_action('init', 'my_block_pattern');
+}
+
 ?>
